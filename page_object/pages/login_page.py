3679c72d95a76f412e4bf42c5a51ecbede334d76
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-09 14:48:42 +0300

    bug fix removed round brackets

--- page_object/pages/login_page.py
+++ page_object/pages/login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        current_url = self.browser.current_url()
+        current_url = self.browser.current_url
         assert "login" in current_url, '"login" отсутствует в текущем URL'
 
     def should_be_login_form(self):
