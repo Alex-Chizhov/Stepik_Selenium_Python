ccb2e4f16174f0b76f7909084af208c87d2491ce
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-10 18:33:45 +0300

    Changed assertions

--- page_object/pages/product_page.py
+++ page_object/pages/product_page.py
@@ -9,7 +9,7 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         price = self.browser.find_element(*ProductPageLocators.PRICE).text
-        main_message = self.browser.find_element(*ProductPageLocators.SUCCESSFULLY_ADDED_TO_CART_MASSAGE).text
+        main_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADDED_TO_CART_MASSAGE).text
         price_message = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_MASSAGE).text
-        assert product_name in main_message, 'Корректного имени продукта нет в сообщении о добавлении в корзину'
-        assert price in price_message, 'Корректной цены нет в сообщение о добавлении товара в корзину'
+        assert product_name == main_message, 'Корректного имени продукта нет в сообщении о добавлении в корзину'
+        assert price == price_message, 'Корректной цены нет в сообщение о добавлении товара в корзину'
