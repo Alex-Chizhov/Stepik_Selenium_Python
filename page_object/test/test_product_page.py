3e3d5f1922fa1d9acb5fbb26ab82a3a06938c54c
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-10 19:26:18 +0300

    Added test_add_to_cart_promo with parametrization and test data for it

--- page_object/test/test_product_page.py
+++ page_object/test/test_product_page.py
@@ -1,8 +1,16 @@
 from page_object.pages.product_page import ProductPage
+from page_object.tests_data import promo_links
+import pytest
 
 
 def test_add_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_item()
+
+@pytest.mark.parametrize('link', promo_links)
+def test_add_to_cart_promo(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart_item()
