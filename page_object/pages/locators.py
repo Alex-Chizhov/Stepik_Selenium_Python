e844e6675efdb1e0fb5211aff2dffd4b02ce6b5e
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-10 13:50:11 +0300

    added locators for Product Page

--- page_object/pages/locators.py
+++ page_object/pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRICE = (By.CSS_SELECTOR, "div.product_main .price_color")
+    SUCCESSFULLY_ADDED_TO_CART_MASSAGE = (By.XPATH, "//*[text()[contains(.,'has been added to your basket')]]")
+    TOTAL_PRICE_MASSAGE = (By.XPATH, "//*[text()[contains(.,'Your basket total is now')]]/strong")
