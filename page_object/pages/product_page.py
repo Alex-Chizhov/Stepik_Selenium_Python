dbef4870311ac44990017d22032d6848bc1f544c
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-11 21:18:57 +0300

    Created methods helpers for the class ProductPage

--- page_object/pages/product_page.py
+++ page_object/pages/product_page.py
@@ -6,10 +6,21 @@ class ProductPage(BasePage):
     def add_to_cart_item(self):
         add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         add_to_cart_button.click()
-        self.solve_quiz_and_get_code()
+
+    def check_product_name_with_main_message(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        price = self.browser.find_element(*ProductPageLocators.PRICE).text
         main_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADDED_TO_CART_MASSAGE).text
-        price_message = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_MASSAGE).text
         assert product_name == main_message, 'Корректного имени продукта нет в сообщении о добавлении в корзину'
+
+    def check_price_with_price_message(self):
+        price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        price_message = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_MASSAGE).text
         assert price == price_message, 'Корректной цены нет в сообщение о добавлении товара в корзину'
+
+    def check_main_message_not_present(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_ADDED_TO_CART_MASSAGE), \
+            "Success message is presented, but should not be"
+
+    def check_main_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_ADDED_TO_CART_MASSAGE), \
+            "Success message is not disappeared"
