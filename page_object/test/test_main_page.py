ceccceb790e7e8f20a1ac170266c9a62557390ca
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-12 16:57:46 +0300

    added test_negative_guest_cant_see_product_in_basket_opened_from_main_page

--- page_object/test/test_main_page.py
+++ page_object/test/test_main_page.py
@@ -1,4 +1,5 @@
 from page_object.pages.main_page import MainPage
+from page_object.pages.basket_page import BasketPage
 from page_object.pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
@@ -14,3 +15,12 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_negative_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page_by_button_in_header()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.check_basket_is_empty()
+    basket_page.check_there_is_message_basket_is_empty()
