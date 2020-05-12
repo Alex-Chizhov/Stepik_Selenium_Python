e3499731eb8a5bff80f9ba6d152392f7c48f9840
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-12 18:16:37 +0300

    Added test_guest_cant_see_product_in_basket_opened_from_product_page

--- page_object/test/test_product_page.py
+++ page_object/test/test_product_page.py
@@ -1,5 +1,6 @@
 from page_object.pages.product_page import ProductPage
 from page_object.tests_data import promo_links
+from page_object.pages.basket_page import BasketPage
 import pytest
 
 
@@ -51,3 +52,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page_by_button_in_header()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.check_basket_is_empty()
+    basket_page.check_there_is_message_basket_is_empty()
