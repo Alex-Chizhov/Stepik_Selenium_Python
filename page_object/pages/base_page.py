e189712458d41f6de6859107f6730fa5f6faf691
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-12 16:58:51 +0300

    added method go_to_basket_page_by_button_in_header

--- page_object/pages/base_page.py
+++ page_object/pages/base_page.py
@@ -61,3 +61,7 @@ class BasePage:
     def should_be_login_link(self):
         # * - указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать.
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_basket_page_by_button_in_header(self):
+        basket_button = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
+        basket_button.click()
