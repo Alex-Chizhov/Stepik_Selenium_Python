d573d2f43a5d38cd2899995ff2eb5e2997358d3c
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-13 17:45:12 +0300

    added methods go_to_login_page and register_new_user to LoginPage class

--- page_object/pages/login_page.py
+++ page_object/pages/login_page.py
@@ -1,7 +1,12 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from ..tests_data import user_registration_data
 
 class LoginPage(BasePage):
+
+    def go_to_login_page(self):
+        self.browser.get('http://selenium1py.pythonanywhere.com/en-gb/accounts/login/')
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -16,3 +21,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self):
+        user_data = user_registration_data()
+        email = self.browser.find_element(*LoginPageLocators.EMAIL_INPUT)
+        email.send_keys(user_data[0])
+        password = self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT)
+        password.send_keys(user_data[1])
+        password_confirm = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD_INPUT)
+        password_confirm.send_keys(user_data[1])
+        register_button = self.browser.find_element(*LoginPageLocators.BUTTON_REGISTER)
+        register_button.click()
