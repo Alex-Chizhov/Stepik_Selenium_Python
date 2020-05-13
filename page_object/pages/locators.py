c6b907a2a328da0ff64f48e6fa3dc87a8144ce19
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-13 17:44:23 +0300

    added new locators for LoginPageLocators class

--- page_object/pages/locators.py
+++ page_object/pages/locators.py
@@ -1,17 +1,23 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group a.btn-default")
 
+
 class MainPageLocators:
-    pass
+    THANKS_FOR_REGISTERING_MASSAGE = (By.XPATH, "//div[text()[contains(.,'Thanks for registering')]]")
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_INPUT = (By.CSS_SELECTOR, "#register_form #id_registration-email")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "#register_form #id_registration-password1")
+    CONFIRM_PASSWORD_INPUT = (By.CSS_SELECTOR, "#register_form #id_registration-password2")
+    BUTTON_REGISTER = (By.CSS_SELECTOR, "[name='registration_submit']")
 
 
 class ProductPageLocators:
