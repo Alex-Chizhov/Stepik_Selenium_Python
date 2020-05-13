37da6a9a415e2dd4dd1064343cbcb8087a4aa814
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-13 17:45:46 +0300

    added method should_thanks_for_registering_message to MainPage class

--- page_object/pages/main_page.py
+++ page_object/pages/main_page.py
@@ -1,5 +1,8 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    pass
+
+    def should_thanks_for_registering_message(self):
+        assert self.is_element_present(*MainPageLocators.THANKS_FOR_REGISTERING_MASSAGE), "MASSAGE is not presented"
