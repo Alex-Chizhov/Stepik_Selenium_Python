2a507521eaa3c447c71b4ef7dc59d2e13537d65a
Alex-Chizhov <alexchizhov90@gmail.com>
2020-05-13 17:41:43 +0300

    added function for generation user data (email, password)

--- page_object/tests_data.py
+++ page_object/tests_data.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 
 promo_links = [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -15,3 +16,17 @@ promo_links = [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
 ]
+
+def user_registration_data():
+    symbol = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+               "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+    def get_characters(range_count):
+        characters = []
+        for i in range(range_count):
+            characters += random.choices(symbol)
+        return characters
+
+    email = ''.join(get_characters(8)) + '@gmail.com'
+    password = ''.join(get_characters(10))
+    return (email, password)
